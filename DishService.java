f6c16a843a64052a6f9f7a7c37a8f48f172f878e
2947264639 <165571720+2947264639@users.noreply.github.com>
2024-10-13 17:23:20 +0800

    Update DishService.java

--- DishService.java
+++ DishService.java
@@ -1,47 +1,3 @@
-package com.sky.service;
-
-import com.sky.dto.DishDTO;
-import com.sky.dto.DishPageQueryDTO;
-import com.sky.entity.Dish;
-import com.sky.result.PageResult;
-import com.sky.vo.DishVO;
-
-import java.util.List;
-
-public interface DishService {
-
-    /**
-     * 新增菜品和对应口味
-     * @param dishDTO
-     */
-    public void saveWithFlavor(DishDTO dishDTO);
-
-    /**
-     * 菜品分页查询
-     * @param dishPageQueryDTO
-     * @return
-     */
-    PageResult pageQuery(DishPageQueryDTO dishPageQueryDTO);
-
-    /**
-     * 菜品批量删除
-     * @param ids
-     */
-    void deletBatch(List<Long> ids);
-
-    /**
-     * 根据id查询菜品和对应的口味数据
-     * @param id
-     * @return
-     */
-    DishVO getByIdWithFlavor(Long id);
-
-    /**
-     * 根据id修改菜品基本信息和对应口味信息
-     * @param dishDTO
-     */
-    void updateWithFlavor(DishDTO dishDTO);
-
     /**
      * 根据分类id查询菜品
      * @param categoryId
@@ -55,4 +11,3 @@ public interface DishService {
      * @param id
      */
     void startOrStop(Integer status, Long id);
-}
