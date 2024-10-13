cf108577ba25c6e87b666c355faf98f7027b9aa2
2947264639 <165571720+2947264639@users.noreply.github.com>
2024-10-13 17:29:42 +0800

    Update SetmealDishMapper.java

--- SetmealDishMapper.java
+++ SetmealDishMapper.java
@@ -1,22 +1,3 @@
-package com.sky.mapper;
-
-import com.sky.entity.SetmealDish;
-import org.apache.ibatis.annotations.Delete;
-import org.apache.ibatis.annotations.Mapper;
-import org.apache.ibatis.annotations.Select;
-
-import java.util.List;
-
-@Mapper
-public interface SetmealDishMapper {
-
-    /**
-     * 根据菜品id查询对应的套餐id
-     * @param dishIds
-     * @return
-     */
-    List<Long> getSetmealIdsByDishIds(List<Long> dishIds);
-
     /**
      * 批量保存菜单和菜品的关联关系
      * @param setmealDishes
@@ -37,4 +18,3 @@ public interface SetmealDishMapper {
      */
     @Select("select * from setmeal_dish where setmeal_id = #{setmealId}")
     List<SetmealDish> getBySetmealId(Long id);
-}
